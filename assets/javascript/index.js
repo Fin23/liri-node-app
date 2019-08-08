5d754e737ef28305ae05286a7670a54a9dd15cf8
Ross Jameson <Rossjameson1623@gmail.com>
2019-08-08 15:27:42 -0500

    finishing initital file set up

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -1,4 +1,16 @@
+//not sure if this should go in the doc.ready fn. 
+require("dotenv").config();
+var keys = require("./keys.js");
+var spotify = new Spotify(keys.spotify);
+
+//Make it so liri.js can take in one of the following commands:
+// concert-this
+// spotify-this-song
+// movie-this
+// do-what-it-says
+
 $(document).ready(function(){
+
 
 
 
