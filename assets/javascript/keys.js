0a9d48832f7d19fe37584f8f118c77febaaf19d5
Ross Jameson <Rossjameson1623@gmail.com>
2019-08-10 20:34:17 -0500

    finishing set up

--- assets/javascript/keys.js
+++ assets/javascript/keys.js
@@ -1,15 +1,28 @@
- 
- 
- console.log('this is loaded');
- 
-var env = require('dotenv').config();
 
 
- exports.spotify = {
-   id: process.env.SPOTIFY_ID,
-   secret: process.env.SPOTIFY_SECRET
+// keys.js
+// You only need to load dotenv once on your main file
+// the process object it a global object and will be available in all your modules
+// var env = require('dotenv').config();
+
+// exports.spotify = {
+//   id: process.env.SPOTIFY_ID,
+//   secret: process.env.SPOTIFY_SECRET
+// };
+
+// exports.omdb = {
+//   api_key: process.env.OMDB_API_KEY
+// };
+
+// Alternatively you do it like this, which feels a little bit cleaner to me.
+// Your choice.
+console.log('this is loaded');
+ module.exports = {
+  spotify: {
+    id: process.env.SPOTIFY_ID,
+     secret: process.env.SPOTIFY_SECRET
+   },
+   omdb: {
+     api_key: process.env.OMDB_API_KEY
+   }
  };
-exports.omdb = {
-  api_key: process.env.OMDB_API_KEY
-};
-
